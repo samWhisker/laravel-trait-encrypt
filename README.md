64ffd4e81b51bdcd57b79d0877e89ba479b4d7f3
samWhisker <sam@koodoocreative.co.uk>
2016-04-12 22:03:48 +0100

    Encryptable trait
    Encryptable trait

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # laravel-trait-encrypt
 A Laravel model trait for Encrypting modal data
+
+
+"koodoo/laravel-trait-encrypt": "dev-master"
+
+use Koodoo\laravelTraitEncrypt\Encryptable;
+use Encryptable;
+
+protected $encryptable = [
+ 'value'
+];
--- src/EncriptTrait.php
+++ src/EncriptTrait.php
@@ -1,10 +1,26 @@
 <?php
 namespace Koodoo\laravelTraitEncrypt;
 
-trait EncriptTrait
+trait Encryptable
 {
-    protected static function bootEncriptTrait()
-    {
-        dd('EncriptTrait');
-    }
+  public function getAttribute($key)
+   {
+
+       $value = parent::getAttribute($key);
+
+       if (in_array($key, $this->encryptable)) {
+           $value = \Crypt::decrypt($value);
+       }
+
+       return $value;
+   }
+
+   public function setAttribute($key, $value)
+   {
+       if (in_array($key, $this->encryptable)) {
+           $value = \Crypt::encrypt($value);
+       }
+
+       return parent::setAttribute($key, $value);
+   }
 }
